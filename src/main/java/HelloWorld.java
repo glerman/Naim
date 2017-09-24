79c55d8b98164b1ba0f49a177ddc8aa98b108b3f
Gal Lerman <glerman@outbrain.com>
2017-09-24 22:11:46 +0300

    Hello World commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by glerman on 24/9/17.
+ */
+public class HelloWorld {
+
+  public static void main(String[] args) {
+
+    System.out.println();
+    System.out.println("Hellow World");
+  }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by glerman on 24/9/17.
+ */
+public class HelloWorldTest {
+
+  @Test
+  public void test() throws Exception {
+
+    HelloWorld.main(null);
+  }
+}
